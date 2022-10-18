43a6c17c98c1a54164cd14afecfa95f7f6ecefe8
flex-akin <akintolafelix2121@gmail.com>
2022-10-18 14:17:05 +0100

    coreectional commit

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const express = require("express")
-const app = 
+const express = require('express')
+const app = express()
+const port = 5010
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
